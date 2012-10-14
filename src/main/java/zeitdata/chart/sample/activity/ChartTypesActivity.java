f3c25cb58d38f9f55461091a2bedf3ba91b33786
scott <scott@zeitdata.com>
2012-10-14 17:39:49 -0500

    initial dev

--- src/main/java/zeitdata/chart/sample/activity/ChartTypesActivity.java
+++ src/main/java/zeitdata/chart/sample/activity/ChartTypesActivity.java
@@ -1,10 +1,46 @@
 package zeitdata.chart.sample.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+import zeitdata.chart.R;
 
+@ContentView(R.layout.charttypes)
 public class ChartTypesActivity extends Activity {
+
+    @InjectView(R.id.lineChartButton)
+    private ImageButton lineChartButton;
+
+    @InjectView(R.id.scatterChartButton)
+    private ImageButton scatterChartButton;
+
+    @InjectView(R.id.barChartButton)
+    private ImageButton barChartButton;
+
+    @InjectView(R.id.pieChartButton)
+    private ImageButton pieChartButton;
+
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        lineChartButton.setOnClickListener(new LineChartButtonListener());
+
     }
+
+
+    private class LineChartButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(ChartTypesActivity.this, LineChartActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+
 }
---
+++ src/main/java/zeitdata/chart/sample/activity/LineChartActivity.java
@@ -0,0 +1,16 @@
+package zeitdata.chart.sample.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import roboguice.inject.ContentView;
+import zeitdata.chart.R;
+
+
+@ContentView(R.layout.linechart)
+public class LineChartActivity extends Activity{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
