4fc099d6927445e8cf6560e1a04d9acdb47b4eef
scott <scott@zeitdata.com>
2012-10-14 16:33:46 -0500

    shell

--- src/main/java/zeitdata/chart/sample/activity/ChartTypesActivity.java
+++ src/main/java/zeitdata/chart/sample/activity/ChartTypesActivity.java
@@ -3,13 +3,6 @@ package zeitdata.chart.sample.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * Created with IntelliJ IDEA.
- * User: scott
- * Date: 10/13/12
- * Time: 10:43 AM
- * To change this template use File | Settings | File Templates.
- */
 public class ChartTypesActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
