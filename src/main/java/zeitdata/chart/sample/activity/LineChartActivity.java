d39a2e75d0d3a0c93a3495e5708fce23c4b6856f
scott <scott@zeitdata.com>
2012-10-15 00:31:35 -0500

    ship it.

--- src/main/java/zeitdata/chart/sample/activity/LineChartActivity.java
+++ src/main/java/zeitdata/chart/sample/activity/LineChartActivity.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 import zeitdata.chart.R;
+import zeitdata.chart.view.LineChart;
 
 
 @ContentView(R.layout.linechart)
 public class LineChartActivity extends RoboActivity {
 
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LineChart lineChart = new LineChart(this);
+
+
+
     }
 
 }
