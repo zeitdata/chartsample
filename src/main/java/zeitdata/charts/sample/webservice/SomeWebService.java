1653eb254c26f8dbcffe3bdee91030fd16086f07
scott <scott@zeitdata.com>
2012-12-10 22:18:39 -0800

    add time service

--- src/main/java/zeitdata/charts/sample/webservice/SomeWebService.java
+++ src/main/java/zeitdata/charts/sample/webservice/SomeWebService.java
@@ -1,10 +1,18 @@
 package zeitdata.charts.sample.webservice;
 
+import java.util.Calendar;
 import java.util.Random;
+
+import android.util.Log;
 
 
 public class SomeWebService {
 
+	
+	public SomeWebService() {
+		Log.d("com.acme", "SomeWebService constructor called");
+	}
+	
 	
     public double[][] getData(int numberOfDataPoints){
 
@@ -19,6 +27,7 @@ public class SomeWebService {
     }
 
     
+    
     public double[][] getAsthmaData(int numberOfDataPoints){
     	return getData(numberOfDataPoints);
     }
@@ -26,6 +35,33 @@ public class SomeWebService {
     
     public double[][] getPollenData(int numberOfDataPoints){
     	return getData(numberOfDataPoints);
+    }
+
+    
+    
+    public double[][] getAsthmaDataByDay(int numberOfDataPoints){
+    	return this.getTimeData(numberOfDataPoints);
+    	
+    }
+    
+    
+    public double[][] getPollenDataByDay(int numberOfDataPoints){
+    	return this.getTimeData(numberOfDataPoints);
+    }
+    
+    
+    public double[][] getTimeData(int numberOfDataPoints){
+
+        double[][] xyPoints = new double[numberOfDataPoints][2];
+
+        Calendar now = Calendar.getInstance();
+        
+        for(int i = 0; i<xyPoints.length; i++){
+        	now.add(Calendar.DAY_OF_YEAR, 1);
+            xyPoints[i][0] = now.getTimeInMillis();
+            xyPoints[i][1] = (new Random().nextInt((i+1)*10) + 1) *(i+1);
+        }
+        return xyPoints;
     }
     
 }
