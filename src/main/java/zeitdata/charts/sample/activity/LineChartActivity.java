d32cf6949e1f4e85dbeb7e2f949778047ffdf643
scott <scott@zeitdata.com>
2013-01-12 13:09:53 -0800

    remove buildTimeData

--- src/main/java/zeitdata/charts/sample/activity/LineChartActivity.java
+++ src/main/java/zeitdata/charts/sample/activity/LineChartActivity.java
@@ -8,9 +8,6 @@ import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
 import zeitdata.charts.model.Data;
-import zeitdata.charts.model.NumericData;
-import zeitdata.charts.model.NumericPoint;
-import zeitdata.charts.model.NumericSeries;
 import zeitdata.charts.model.TimeData;
 import zeitdata.charts.model.TimePoint;
 import zeitdata.charts.model.TimeSeries;
@@ -38,50 +35,6 @@ public class LineChartActivity extends RoboActivity {
 
         lineChart.setData(numericPollenAsthmaData);
 
-    }
-
-    
-
-    private NumericData buildNumericData(){
-    	
-    	
-    	//1st series - Week of Year (x) and Asthma Attacks (y)
-    	//get Asthma Data for 15 weeks from someplace
-        double[][] xyAsthmaPoints = someWebService.getAsthmaData(15);
-
-        NumericSeries.Builder asthmaSeriesBuilder = new NumericSeries.Builder();
-
-        asthmaSeriesBuilder.withTitle("Asthma Attacks");
-        //if you do not assign a Color, one will be chosen for you 
-        asthmaSeriesBuilder.withColor(Color.RED);
-        
-        for(int i = 0; i < xyAsthmaPoints.length; i++){
-            asthmaSeriesBuilder.addPoint(new NumericPoint.Builder(xyAsthmaPoints[i][0], xyAsthmaPoints[i][1]).build());
-        }
-        NumericSeries asthmaSeries = asthmaSeriesBuilder.build();
-
-        
-
-        //2nd series - Week of Year (x) and Pollen Count (y) 
-        double[][] xyPollenCount = someWebService.getPollenData(15);
-        
-        NumericSeries.Builder pollenSeriesBuilder = new NumericSeries.Builder();
-        
-        pollenSeriesBuilder.withTitle("Pollen Count");
-        asthmaSeriesBuilder.withColor(Color.rgb(165,42,42));  //brown
-        
-        //add data to the PollenSeriesBuilder
-        for(int i = 0; i < xyPollenCount.length; i++){
-        	pollenSeriesBuilder.addPoint(new NumericPoint.Builder(xyPollenCount[i][0], xyPollenCount[i][1]).build());
-        }
-        
-        NumericSeries pollenSeries = pollenSeriesBuilder.build();
-        
-        NumericData.Builder numericDataBuilder = new NumericData.Builder();
-        numericDataBuilder.addSeries(asthmaSeries, pollenSeries);
-        NumericData numericData = numericDataBuilder.build(); 
-       
-        return numericData;
     }
 
 
@@ -129,4 +82,49 @@ public class LineChartActivity extends RoboActivity {
 
         return timeData;
     }
+
+
+    /*
+    private NumericData buildNumericData(){
+
+
+        //1st series - Week of Year (x) and Asthma Attacks (y)
+        //get Asthma Data for 15 weeks from someplace
+        double[][] xyAsthmaPoints = someWebService.getAsthmaData(15);
+
+        NumericSeries.Builder asthmaSeriesBuilder = new NumericSeries.Builder();
+
+        asthmaSeriesBuilder.withTitle("Asthma Attacks");
+        //if you do not assign a Color, one will be chosen for you
+        asthmaSeriesBuilder.withColor(Color.RED);
+
+        for(int i = 0; i < xyAsthmaPoints.length; i++){
+            asthmaSeriesBuilder.addPoint(new NumericPoint.Builder(xyAsthmaPoints[i][0], xyAsthmaPoints[i][1]).build());
+        }
+        NumericSeries asthmaSeries = asthmaSeriesBuilder.build();
+
+
+
+        //2nd series - Week of Year (x) and Pollen Count (y)
+        double[][] xyPollenCount = someWebService.getPollenData(15);
+
+        NumericSeries.Builder pollenSeriesBuilder = new NumericSeries.Builder();
+
+        pollenSeriesBuilder.withTitle("Pollen Count");
+        asthmaSeriesBuilder.withColor(Color.rgb(165,42,42));  //brown
+
+        //add data to the PollenSeriesBuilder
+        for(int i = 0; i < xyPollenCount.length; i++){
+            pollenSeriesBuilder.addPoint(new NumericPoint.Builder(xyPollenCount[i][0], xyPollenCount[i][1]).build());
+        }
+
+        NumericSeries pollenSeries = pollenSeriesBuilder.build();
+
+        NumericData.Builder numericDataBuilder = new NumericData.Builder();
+        numericDataBuilder.addSeries(asthmaSeries, pollenSeries);
+        NumericData numericData = numericDataBuilder.build();
+
+        return numericData;
+    }
+    */
 }
